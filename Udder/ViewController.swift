ef89a945ef88de97274f5c4d1b6477135f21281a
Joseph Ray <Josephtyndaleray@gmail.com>
2019-06-25 14:03:16 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Joseph Ray on 6/25/19.
+//  Copyright © 2019 Joseph Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
